ad16bef17ce091e56cee4de81498252284c3056f
AndrewRaySykes <andrewraysykes@gmail.com>
2014-05-30 16:07:22 -0400

    Friends Activity Done
    Full Friends Add/Remove/Display Functionality
    Next step: CAMERA!

---
+++ src/com/andrewraysykes/ribbit/ParseConstants.java
@@ -0,0 +1,14 @@
+package com.andrewraysykes.ribbit;
+
+public final class ParseConstants {
+	
+	//Values
+	public static final Integer KEY_FRIENDS_SEARCH_LIMIT = 1000;
+	
+	//Class names
+	
+	
+	//Field names
+	public static final String KEY_USERNAME = "username";
+	public static final String KEY_FRIENDS_RELATION = "friendsRelation";
+}
