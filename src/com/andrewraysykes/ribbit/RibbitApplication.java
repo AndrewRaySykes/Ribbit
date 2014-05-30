6ea4d2622aeb3ea985225fbb7f4fb9c6a1e5ac06
AndrewRaySykes <andrewraysykes@gmail.com>
2014-05-30 12:07:57 -0400

    Login/Signup Functionality
    Parse connected. Login and Signup and Logout Features fully functional!

---
+++ src/com/andrewraysykes/ribbit/RibbitApplication.java
@@ -0,0 +1,18 @@
+package com.andrewraysykes.ribbit;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+public class RibbitApplication extends Application {
+	
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		Parse.initialize(this, "Tv8bIwqegGXuOQfMSWdO5BRK1R6bjGXN6E5mDzFh", "mqt60ZnWQeK9jILYcnwaJTP8yGinPJQvh0wGGxzA");
+		
+		}
+	
+
+}
